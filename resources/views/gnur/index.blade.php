adcf112dc20bdb60569c2cbdc28370c8eaf44537
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-06-01 07:59:19 +0600

    guru template add

---
+++ resources/views/gnur/index.blade.php
@@ -0,0 +1,10 @@
+@extends('gnur.layout', ['title' => 'Todo List'])
+@section('head')
+
+@endsection
+@section('content')
+
+    @include('gnur.inc.nav')
+    @include('gnur.inc.main')
+    @include('gnur.inc.button')
+@endsection
